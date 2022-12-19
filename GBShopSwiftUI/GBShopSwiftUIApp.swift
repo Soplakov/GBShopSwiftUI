cf007233211afae94a78b547f54bde4f9558f81c
Soplakov <soplakov@gmail.com>
2022-12-20 01:09:01 +0500

    Initial Commit

---
+++ GBShopSwiftUI/GBShopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBShopSwiftUIApp.swift
+//  GBShopSwiftUI
+//
+//  Created by PiZero on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GBShopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
